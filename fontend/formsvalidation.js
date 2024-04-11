459b92f8857ec3f285fbbf3aed6bfa7167fd4b19
apsilvanatalia <apsilva.natalia@gmail.com>
2024-04-11 07:35:07 -0300

    correcao final

--- fontend/formsvalidation.js
+++ fontend/formsvalidation.js
@@ -1,4 +1,13 @@
-$(document).ready(function(){
-    $('#phone').mask('(00) 0000-0000');
-    
-  });
+function applyMasks() {
+    $('#phone').mask('(00) 0 0000-0000');
+    $('#state').mask('AA', {
+        translation: {
+            'A': { pattern: /[a-zA-Z]/ }
+        }
+    });
+    $('#state').on('input', function() {
+        $(this).val(function(_, val) {
+            return val.toUpperCase();
+        });
+    });
+}
